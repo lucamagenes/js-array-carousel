f4cff007e6047b37b7e4b32d837775388f55ed57
lucamagenes <lucamgns33@gmail.com>
2021-10-15 15:35:13 +0200

    struttura logica js

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+//creare un gruppo di valori che corrisponda a ciascuna immagine e testi
+
+/*
+
+assegnare ai bottoni le funzioni per selezionare le immagini
+- creare un eventlistener
+- creare un loop for
+- freccia GIU seleziona l'immagine sottostante
+- freccia SU seleziona l'immagine soprastante
+
+*/
+
+
+
+/*
+
+se la miniatura viene selezionata:
+- viene aggiunto un bordo alla miniatura
+- l'immagine viene visualizata nell'anteprima
+    - vengono selezionati i testi relativi all'immagine selezionata
+
+altrimenti le miniature non selezionate:
+- risultano oscurate
+
+*/
